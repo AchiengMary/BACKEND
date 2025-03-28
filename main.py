98b21b477bbd4e7b083371919d7affd09663d008
JoshMusira <joshuamusira01@gmail.com>
2025-03-28 19:57:30 +0300

    feat: Project initialization

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+     title="Solar Hot Water System API",
+    description="This API assists in configuring the optimal solar hot water system by guiding users through the triaging and sizing processes. The API dynamically recommends system configurations based on user input, helping to generate tailored proposals and quotations.",
+    
+)
+
+# Add cors middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(guest_router)
+
+
+@app.get('/')
+def health_check():
+    return JSONResponse(content={"status": "Wellcome. The server is up and Running!"})
