1577ecec577291297547466a8bdd099fbe24a9be
Achieng Mary <achieng75mary@gmail.com>
2025-05-13 11:19:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # Add cors middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://davisandshirtliffsolarwaterassistant.vercel.app", "http://localhost:5173"],
+    allow_origins=["https://f1-f.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
